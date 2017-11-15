71a8499797a21a1089318ea60e2029feb72c49bb
Ferenc Bolla <bollaferenc@gmail.com>
2017-11-15 09:58:52 +0100

    shout()

--- gittie.py
+++ gittie.py
@@ -1,10 +1,7 @@
 def introduce():
-<<<<<<< HEAD
     print("Hello, I'm Gittie!")
     print("talicska")
-=======
     print("Hello, I'm Gittie! asdasd")
->>>>>>> 643b4b6ad9760f0e24cc319ed39935127a94f0f3
 
 
 def add(a, b):
@@ -12,6 +9,10 @@ def add(a, b):
 
 
 def joke():
+    pass
+
+
+def shout():
     pass
 
 """
